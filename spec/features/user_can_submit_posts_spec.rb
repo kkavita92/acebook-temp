8e5be15401407264280cff863b560d4fc7394fa7
Kavita <kkavita92@gmail.com>
2017-06-28 23:30:27 +0100

    Post is displayed with User next to it

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
+
   scenario "Can submit posts and view them" do
     sign_in
     visit "/posts"
@@ -8,5 +9,6 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+    expect(page).to have_content("user10@example.com")
   end
 end
