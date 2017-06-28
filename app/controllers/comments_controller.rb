93fb1a9ea301dc6df4c402ec4ff9fd32773c15b8
Kavita <kkavita92@gmail.com>
2017-06-29 00:10:54 +0100

    - Link User model to Comments - Allow Comments to be posted with name of user who wrote it

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :user_id)
     end
 end
