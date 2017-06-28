6765c5b971d06f19115cbacff563d78dbc99c1bc
Kavita <kkavita92@gmail.com>
2017-06-28 23:35:30 +0100

    Refactor feature tests for submitting post

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
 
-  scenario "Can submit posts and view them" do
+  before(:each) do
     sign_in
+  end
+
+  scenario "Can submit posts and view them" do
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
-    expect(page).to have_content("user10@example.com")
+  end
+
+  scenario "Can view user that submitted post" do
+    visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello again"
     click_button "Submit"
-    expect(page).to have_content("Hello again by user10@example.com")
+    expect(page).to have_content("Hello again by user11@example.com")
   end
+
 end
