78cdec0c8b542068718b222ed9fc8eea057ef1f7
Kavita <kkavita92@gmail.com>
2017-06-29 01:52:37 +0100

    User can delete their own comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body, :user_id)
